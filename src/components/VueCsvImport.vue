a7ad544ac939d9bf4ec930c8755495d3c8228b41
BorceZ <boriszarinski@outlook.com>
2021-04-04 20:05:01 +0200

    bugfixes

--- src/components/VueCsvImport.vue
+++ src/components/VueCsvImport.vue
@@ -191,9 +191,12 @@
                     console.log(_this.csv);
                     for (const [index, [key, value]] of Object.entries(Object.entries(_this.csv[0]))) {
                         console.log(`${index}: ${key} = ${value}`);
-                    }
-
+                        let jsObj = {};
+                        jsObj[value] = key;
+                        _this.map.push(jsObj)}
                 });
+                console.log("MAP AFTER LOAD");
+                console.log(_this.map);
                 this.$emit('loadedData', true);
 
             },
